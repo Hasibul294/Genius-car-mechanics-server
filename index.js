cbb748dc63b22d466996c015a4e3d9febacb5379
Hasibul294 <hasibulhasan294@gmail.com>
2021-10-27 16:08:45 +0600

    port added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const ObjectId = require("mongodb").ObjectId;
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
